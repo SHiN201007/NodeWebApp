2bd9a5a6ef0ff93e496d1996c852dfe4a767c2a3
watababe-bc-work <watanabe.bc.work@gmail.com>
2022-07-07 14:49:35 +0900

    Firebase configを別ファイルで管理

--- public/javascripts/Models/SignModel.js
+++ public/javascripts/Models/SignModel.js
@@ -1,25 +1,8 @@
 const express = require('express');
 require('../Extensions/StringExtension');
+require('../Extensions/firebase');
 
-const firebase = require("firebase/app");
 const firebaseAuth = require("firebase/auth");
-
-const config = {
-  apiKey: 'AIzaSyAAERLaSes1Tl24GypkwswudGbUFOSbp9w',
-  authDomain: 'snaphy-3bf68.firebaseapp.com'
-};
-firebase.initializeApp(config);
-
-// module.exports = {
-//     // サインアップ処理
-//     signUp: function(email,password) {
-//         return new Promise((resolve, reject) => {
-//             const emailStr = String(email);
-//             const result = emailStr.isEmpty() ? 'failure' : 'success';
-//             resolve(result);
-//         });
-//     }
-// }
 
 module.exports = {
     // サインアップ処理
